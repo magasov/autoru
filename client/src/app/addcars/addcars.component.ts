a02ec80f9ef2190736c74169ffd0fcea4ec075cb
magasov <77aushev@mail.ru>
2025-05-09 16:08:08 +0300

    addcars full logic

--- client/src/app/addcars/addcars.component.ts
+++ client/src/app/addcars/addcars.component.ts
@@ -60,274 +60,83 @@ export class AddcarsComponent {
 
   brandModels: { [key: string]: string[] } = {
     'Lada (ВАЗ)': [
-      '2101',
-      '2104',
-      '2105',
-      '2106',
-      '2107',
-      '2109',
-      '21099',
-      '2110',
-      '2111',
-      '2112',
-      '2113',
-      '2114',
-      '2115',
-      '2121 (4x4)',
-      '2131 (4x4)',
-      'Granta',
-      'Kalina',
-      'Largus',
-      'Niva',
-      'Niva Legend',
-      'Priora',
-      'Vesta',
-      'XRAY',
+      '2101', '2104', '2105', '2106', '2107', '2109', '21099', '2110', '2111', '2112', '2113', '2114', '2115',
+      '2121 (4x4)', '2131 (4x4)', 'Granta', 'Kalina', 'Largus', 'Niva', 'Niva Legend', 'Priora', 'Vesta', 'XRAY',
     ],
     Audi: [
-      '100',
-      '80',
-      'A1',
-      'A3',
-      'A4',
-      'A4 allroad',
-      'A5',
-      'A6',
-      'A6 allroad',
-      'A7',
-      'A8',
-      'Q3',
-      'Q5',
-      'Q5 Sportback',
-      'Q7',
-      'Q8',
-      'RS 5',
-      'RS Q8',
-      'S5',
-      'S8',
-      'SQ5',
-      'TT',
-      'e-tron',
+      '100', '80', 'A1', 'A3', 'A4', 'A4 allroad', 'A5', 'A6', 'A6 allroad', 'A7', 'A8', 'Q3', 'Q5', 'Q5 Sportback',
+      'Q7', 'Q8', 'RS 5', 'RS Q8', 'S5', 'S8', 'SQ5', 'TT', 'e-tron',
     ],
     Bmw: [
-      '1 серии',
-      '2 серии',
-      '3 серии',
-      '4 серии',
-      '5 серии',
-      '6 серии',
-      '7 серии',
-      '8 серии',
-      'M3',
-      'M4',
-      'M5',
-      'X1',
-      'X2',
-      'X3',
-      'X3 M',
-      'X4',
-      'X5',
-      'X5 M',
-      'X6',
-      'X6 M',
-      'X7',
-      'Z4',
-      'i3',
+      '1 серии', '2 серии', '3 серии', '4 серии', '5 серии', '6 серии', '7 серии', '8 серии', 'M3', 'M4', 'M5',
+      'X1', 'X2', 'X3', 'X3 M', 'X4', 'X5', 'X5 M', 'X6', 'X6 M', 'X7', 'Z4', 'i3',
     ],
     Hyundai: [
-      'Accent',
-      'Avante',
-      'Creta',
-      'Elantra',
-      'Getz',
-      'Grand Starex',
-      'Grandeur',
-      'H-1',
-      'Kona',
-      'Matrix',
-      'Palisade',
-      'Santa Fe',
-      'Solaris',
-      'Sonata',
-      'Starex',
-      'Staria',
-      'Terracan',
-      'Tucson',
-      'Veloster',
-      'i20',
-      'i30',
-      'i40',
-      'ix35',
+      'Accent', 'Avante', 'Creta', 'Elantra', 'Getz', 'Grand Starex', 'Grandeur', 'H-1', 'Kona', 'Matrix', 'Palisade',
+      'Santa Fe', 'Solaris', 'Sonata', 'Starex', 'Staria', 'Terracan', 'Tucson', 'Veloster', 'i20', 'i30', 'i40', 'ix35',
     ],
     Kia: [
-      'Carens',
-      'Carnival',
-      'Ceed',
-      'Cerato',
-      'Forte',
-      'K3',
-      'K5',
-      'K7',
-      'K8',
-      'Magentis',
-      'Mohave',
-      'Morning',
-      'Optima',
-      'Picanto',
-      'Quoris',
-      'Rio',
-      'Seltos',
-      'Sorento',
-      'Soul',
-      'Spectra',
-      'Sportage',
-      'Stinger',
-      'Venga',
+      'Carens', 'Carnival', 'Ceed', 'Cerato', 'Forte', 'K3', 'K5', 'K7', 'K8', 'Magentis', 'Mohave', 'Morning',
+      'Optima', 'Picanto', 'Quoris', 'Rio', 'Seltos', 'Sorento', 'Soul', 'Spectra', 'Sportage', 'Stinger', 'Venga',
     ],
     Mercedes: [
-      'A-Класс',
-      'AMG GT',
-      'B-Класс',
-      'C-Класс',
-      'CLA',
-      'CLS',
-      'E-Класс',
-      'G-Класс',
-      'G-Класс AMG',
-      'GL-Класс',
-      'GLA',
-      'GLC',
-      'GLC Coupe',
-      'GLE',
-      'GLE Coupe',
-      'GLE Coupe AMG',
-      'GLK-Класс',
-      'GLS',
-      'M-Класс',
-      'Maybach S-Класс',
-      'S-Класс',
-      'V-Класс',
-      'Vito',
+      'A-Класс', 'AMG GT', 'B-Класс', 'C-Класс', 'CLA', 'CLS', 'E-Класс', 'G-Класс', 'G-Класс AMG', 'GL-Класс', 'GLA',
+      'GLC', 'GLC Coupe', 'GLE', 'GLE Coupe', 'GLE Coupe AMG', 'GLK-Класс', 'GLS', 'M-Класс', 'Maybach S-Класс', 'S-Класс',
+      'V-Класс', 'Vito',
     ],
     Nissan: [
-      'AD',
-      'Almera',
-      'Almera Classic',
-      'Cube',
-      'Juke',
-      'Leaf',
-      'March',
-      'Maxima',
-      'Micra',
-      'Murano',
-      'Note',
-      'Pathfinder',
-      'Patrol',
-      'Primera',
-      'Qashqai',
-      'Qashqai+2',
-      'Serena',
-      'Sunny',
-      'Teana',
-      'Terrano',
-      'Tiida',
-      'Wingroad',
-      'X-Trail',
+      'AD', 'Almera', 'Almera Classic', 'Cube', 'Juke', 'Leaf', 'March', 'Maxima', 'Micra', 'Murano', 'Note', 'Pathfinder',
+      'Patrol', 'Primera', 'Qashqai', 'Qashqai+2', 'Serena', 'Sunny', 'Teana', 'Terrano', 'Tiida', 'Wingroad', 'X-Trail',
     ],
     Renault: [
-      'Arkana',
-      'Clio',
-      'Dokker',
-      'Duster',
-      'Espace',
-      'Fluence',
-      'Kadjar',
-      'Kangoo',
-      'Kaptur',
-      'Koleos',
-      'Laguna',
-      'Latitude',
-      'Logan',
-      'Megane',
-      'Sandero',
-      'Scenic',
-      'Symbol',
-      'Talisman',
-      'Trafic',
+      'Arkana', 'Clio', 'Dokker', 'Duster', 'Espace', 'Fluence', 'Kadjar', 'Kangoo', 'Kaptur', 'Koleos', 'Laguna',
+      'Latitude', 'Logan', 'Megane', 'Sandero', 'Scenic', 'Symbol', 'Talisman', 'Trafic',
     ],
     Toyota: [
-      'Allion',
-      'Alphard',
-      'Auris',
-      'Avensis',
-      'C-HR',
-      'Caldina',
-      'Camry',
-      'Corolla',
-      'Corona',
-      'Crown',
-      'Harrier',
-      'Highlander',
-      'Hilux',
-      'Land Cruiser',
-      'Land Cruiser Prado',
-      'Mark II',
-      'Passo',
-      'Prius',
-      'RAV4',
-      'Sienta',
-      'Vitz',
-      'Wish',
-      'Yaris',
+      'Allion', 'Alphard', 'Auris', 'Avensis', 'C-HR', 'Caldina', 'Camry', 'Corolla', 'Corona', 'Crown', 'Harrier',
+      'Highlander', 'Hilux', 'Land Cruiser', 'Land Cruiser Prado', 'Mark II', 'Passo', 'Prius', 'RAV4', 'Sienta', 'Vitz',
+      'Wish', 'Yaris',
     ],
     Skoda: [
-      'Fabia',
-      'Felicia',
-      'Kamiq',
-      'Karoq',
-      'Kodiaq',
-      'Kodiaq GT',
-      'Octavia',
-      'Octavia RS',
-      'Rapid',
-      'Roomster',
-      'SuperAdvisors',
-      'Yeti',
+      'Fabia', 'Felicia', 'Kamiq', 'Karoq', 'Kodiaq', 'Kodiaq GT', 'Octavia', 'Octavia RS', 'Rapid', 'Roomster',
+      'SuperAdvisors', 'Yeti',
     ],
     Volkswagen: [
-      'Amarok',
-      'Arteon',
-      'Bora',
-      'Caddy',
-      'Caravelle',
-      'Golf',
-      'Golf GTI',
-      'Golf Plus',
-      'ID.4',
-      'Jetta',
-      'Multivan',
-      'Passat',
-      'Passat (North America and China)',
-      'Passat CC',
-      'Polo',
-      'Sharan',
-      'Talagon',
-      'Tayron',
-      'Teramont',
-      'Tiguan',
-      'Touareg',
-      'Touran',
-      'Transporter',
+      'Amarok', 'Arteon', 'Bora', 'Caddy', 'Caravelle', 'Golf', 'Golf GTI', 'Golf Plus', 'ID.4', 'Jetta', 'Multivan',
+      'Passat', 'Passat (North America and China)', 'Passat CC', 'Polo', 'Sharan', 'Talagon', 'Tayron', 'Teramont',
+      'Tiguan', 'Touareg', 'Touran', 'Transporter',
     ],
   };
 
-  
   years: string[] = [
     '2025', '2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017',
     '2016', '2015', '2014', '2013', '2012', '2011', 'Старше'
   ];
 
-  
+  bodyTypes: string[] = [
+    'Седан', 'Хэтчбек', 'Универсал', 'Внедорожник', 'Купе', 'Кабриолет', 'Минивэн', 'Пикап'
+  ];
+
+  generations: string[] = [
+    'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'Другое'
+  ];
+
+  engines: string[] = [
+    'Бензин', 'Дизель', 'Гибрид', 'Электрический'
+  ];
+
+  drivetrains: string[] = [
+    'Передний', 'Задний', 'Полный'
+  ];
+
+  transmissions: string[] = [
+    'Механика', 'Автомат', 'Робот', 'Вариатор'
+  ];
+
+  modifications: string[] = [
+    '150 л.с. (2.0 AMT)', '200 л.с. (2.5 AT)', '100 л.с. (1.6 MT)', 'Другая'
+  ];
+
   colors: string[] = [
     'Белый', 'Черный', 'Серебристый', 'Серый', 'Синий', 'Красный', 'Зеленый',
     'Желтый', 'Оранжевый', 'Коричневый', 'Другой'
@@ -339,7 +148,14 @@ export class AddcarsComponent {
   modelSearchTerm: string = '';
   filteredModels: string[] = [];
   selectedModel: string | null = null;
-  selectedYear: string | null = null; 
+  selectedYear: string | null = null;
+  selectedBodyType: string | null = null;
+  selectedGeneration: string | null = null;
+  selectedEngine: string | null = null;
+  selectedDrivetrain: string | null = null;
+  selectedTransmission: string | null = null;
+  selectedModification: string | null = null;
+  selectedColor: string | null = null;
 
   filterBrands() {
     const search = this.searchTerm.toLowerCase().trim();
@@ -354,8 +170,7 @@ export class AddcarsComponent {
 
   selectBrand(brandName: string) {
     this.selectedBrand = brandName;
-    this.selectedModel = null;
-    this.selectedYear = null; 
+    this.resetAfterBrand();
     this.searchTerm = '';
     this.filteredBrands = [...this.brands];
     this.modelSearchTerm = '';
@@ -364,13 +179,48 @@ export class AddcarsComponent {
 
   selectModel(modelName: string) {
     this.selectedModel = modelName;
-    this.selectedYear = null; 
+    this.resetAfterModel();
     this.modelSearchTerm = modelName;
     this.filteredModels = [];
   }
 
   selectYear(year: string) {
-    this.selectedYear = year; 
+    this.selectedYear = year;
+    this.resetAfterYear();
+  }
+
+  selectBodyType(bodyType: string) {
+    this.selectedBodyType = bodyType;
+    this.resetAfterBodyType();
+  }
+
+  selectGeneration(generation: string) {
+    this.selectedGeneration = generation;
+    this.resetAfterGeneration();
+  }
+
+  selectEngine(engine: string) {
+    this.selectedEngine = engine;
+    this.resetAfterEngine();
+  }
+
+  selectDrivetrain(drivetrain: string) {
+    this.selectedDrivetrain = drivetrain;
+    this.resetAfterDrivetrain();
+  }
+
+  selectTransmission(transmission: string) {
+    this.selectedTransmission = transmission;
+    this.resetAfterTransmission();
+  }
+
+  selectModification(modification: string) {
+    this.selectedModification = modification;
+    this.resetAfterModification();
+  }
+
+  selectColor(color: string) {
+    this.selectedColor = color;
   }
 
   filterModels() {
@@ -387,10 +237,55 @@ export class AddcarsComponent {
 
   clearSelection() {
     this.selectedBrand = null;
-    this.selectedModel = null;
-    this.selectedYear = null; 
+    this.resetAfterBrand();
+    this.searchTerm = '';
     this.modelSearchTerm = '';
     this.filteredModels = [];
+    this.filteredBrands = [...this.brands];
+  }
+
+  resetAfterBrand() {
+    this.selectedModel = null;
+    this.resetAfterModel();
+  }
+
+  resetAfterModel() {
+    this.selectedYear = null;
+    this.resetAfterYear();
+  }
+
+  resetAfterYear() {
+    this.selectedBodyType = null;
+    this.resetAfterBodyType();
+  }
+
+  resetAfterBodyType() {
+    this.selectedGeneration = null;
+    this.resetAfterGeneration();
+  }
+
+  resetAfterGeneration() {
+    this.selectedEngine = null;
+    this.resetAfterEngine();
+  }
+
+  resetAfterEngine() {
+    this.selectedDrivetrain = null;
+    this.resetAfterDrivetrain();
+  }
+
+  resetAfterDrivetrain() {
+    this.selectedTransmission = null;
+    this.resetAfterTransmission();
+  }
+
+  resetAfterTransmission() {
+    this.selectedModification = null;
+    this.resetAfterModification();
+  }
+
+  resetAfterModification() {
+    this.selectedColor = null;
   }
 
   getSelectedBrandLogo(): string {
