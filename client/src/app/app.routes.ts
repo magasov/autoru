2998803f43935367a125efb633ce8b3fb534fff8
magasov <77aushev@mail.ru>
2025-05-10 08:18:06 +0300

    design page cars

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AddcarsComponent } from './addcars/addcars.component';
 import { AuthGuard } from './auth.guard';
+import { CarsComponent } from './cars/cars.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileComponent},
-   { path: 'addcars', component: AddcarsComponent, canActivate: [AuthGuard] }
+   { path: 'addcars', component: AddcarsComponent, canActivate: [AuthGuard] },
+   { path: 'cars/:id', component: CarsComponent }
 ];
--- client/src/app/cars/cars.component.ts
+++ client/src/app/cars/cars.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cars',
-  imports: [],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.scss'
 })
