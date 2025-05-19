21b787c55755d8f6495a3fe194c4cbc0a81fb2f9
magasov <77aushev@mail.ru>
2025-05-19 21:44:08 +0300

    web chat dev

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MessageComponent } from "./message/message.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ client/src/app/message/message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-message',
+  imports: [RouterLink],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.scss'
+})
+export class MessageComponent {
+
+}
